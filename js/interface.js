137ee18578033576ab1c06815c70c7b71fc8cced
Hugo Carneiro <hcarneiro@weboo.biz>
2016-08-08 16:54:44 +0100

    image widget changes

--- js/interface.js
+++ js/interface.js
@@ -1,11 +1,5 @@
 var widgetData = Fliplet.Widget.getData();
 
-function getFormData() {
-  return {
-    width: $('[name="width"]').val() || '100%'
-  }
-}
-
 // Fired from Fliplet Studio when the external save button is clicked
 Fliplet.Widget.onSaveRequest(function () {
   Fliplet.Widget.save(getFormData()).then(function () {
@@ -13,23 +7,16 @@ Fliplet.Widget.onSaveRequest(function () {
   });
 })
 
-$('[data-select-image]').click(function (event) {
-  event.preventDefault();
-
-  Fliplet.Widget.open('com.fliplet.file-manager', {
+if (!widgetData) {
+  Fliplet.Widget.open('com.fliplet.image-manager', {
+    selector: "#image-manager",
     single: true,
     type: 'image'
   }).then(function (result) {
-    var data = getFormData();
     data.image = result.data;
 
     return Fliplet.Widget.save(data);
   }).then(function () {
     window.location.reload();
   });
-});
-
-// Open the file manager if no image has been selected
-if (!widgetData) {
-  $('[data-select-image]').click();
-}
+};
