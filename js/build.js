dff20cfdc40d4006396f1a3977c2ed6099495b7a
Ricardo Seromenho <ricardo@seromenho.com>
2016-08-17 18:40:02 +0100

    Fix: on build check if link is set

--- js/build.js
+++ js/build.js
@@ -2,7 +2,7 @@ $('[data-image-id]').click(function (event) {
   event.preventDefault();
 
   var data = Fliplet.Widget.getData($(this).data('image-id'));
-  if (data.link === 'true') {
+  if (data.link) {
     Fliplet.Navigate.to(data.action);
   }
 });
