91f93b07db4ef8d28ae6aa7acba3be94cb0639e5
Tony Wu <tonytlwu@gmail.com>
2017-02-16 14:44:38 +0000

    We hate indentation

--- js/build.js
+++ js/build.js
@@ -3,32 +3,34 @@ Fliplet.Widget.instance('image', function (data) {
   var canvas = this;
   var imageUrl = data.image && data.image.url;
 
-  // Load images after DOM Load
-  if (imageUrl) {
-    Fliplet.Navigator.onReady().then(function () {
-      var $placeholder = $(canvas);
-      var img = document.createElement('IMG');
-      var propsToCopy = ['style', 'className', 'width', 'height'];
-      propsToCopy.forEach(function(x){
-        if (canvas.hasOwnProperty(x)) {
-          img[x] = canvas[x];
-        }
-      });
-      img.dataset.imageId = canvas.dataset.imageId;
-      img.classList.remove('lazy-placeholder');
-      var $img = $(img);
-      $img.on('load', function(){
-        $placeholder.replaceWith(this);
-        $(this).hide().fadeIn(200);
-      }).attr('src', imageUrl);
-
-      $img.on('click', function (event) {
-        event.preventDefault();
-
-        if (data.action) {
-          Fliplet.Navigate.to(data.action);
-        }
-      })
-    });
+  if (!imageUrl) {
+    return;
   }
+
+  // Load images after DOM Load
+  Fliplet.Navigator.onReady().then(function () {
+    var $placeholder = $(canvas);
+    var img = document.createElement('IMG');
+    var propsToCopy = ['style', 'className', 'width', 'height'];
+    propsToCopy.forEach(function(x){
+      if (canvas.hasOwnProperty(x)) {
+        img[x] = canvas[x];
+      }
+    });
+    img.dataset.imageId = canvas.dataset.imageId;
+    img.classList.remove('lazy-placeholder');
+    var $img = $(img);
+    $img.on('load', function(){
+      $placeholder.replaceWith(this);
+      $(this).hide().fadeIn(200);
+    }).attr('src', imageUrl);
+
+    $img.on('click', function (event) {
+      event.preventDefault();
+
+      if (data.action) {
+        Fliplet.Navigate.to(data.action);
+      }
+    })
+  });
 });
