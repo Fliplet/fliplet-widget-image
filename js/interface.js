82bc5fbf9b6ef26c19fc7ed13d597ebee8c6fa2e
Hugo Carneiro <hcarneiro@weboo.biz>
2017-06-16 18:56:07 +0100

    Tidy all up

--- js/interface.js
+++ js/interface.js
@@ -1,70 +1,124 @@
 var widgetId = Fliplet.Widget.getDefaultId();
 var data = Fliplet.Widget.getData() || {};
 
-var linkActionProvider;
+var filePickerProvider;
 var imageEditorProvider;
+var linkActionProvider;
 
-var files = $.extend(data.files, {
-  selectFiles: [],
-  selectMultiple: false,
-  type: 'image',
-  autoSelectOnUpload: true
-});
+var filePickerData;
 
-// Load image editor if iamge is saved
-if (data.image) {
-  initImageEditor();
+function init() {
+  filePickerInit();
+  imageEditorInit();
+
+  // Load link action
+  if (data.action && data.action.action === 'gallery') {
+    $('#pinch').prop('checked', true);
+  }
+
+  if (data.action && data.action.action !== 'gallery') {
+    linkProviderInit(data.action);
+    $('.link-actions').addClass('show');
+    $('#link').prop('checked', true);
+  }
+
+  if (!data.action) {
+    $('#none').prop('checked', true);
+  }
+
+  attahObservers();
 }
 
-// Load link action
-if (data.action && data.action.action === 'gallery') {
-  $('#pinch').prop('checked', true);
-}
-
-if (data.action && data.action.action !== 'gallery') {
-  initLinkProvider(data.action);
-  $('.link-actions').addClass('show');
-  $('#link').prop('checked', true);
-}
-
-if (!data.action) {
-  $('#none').prop('checked', true);
-}
-
-var filePickerProvider = Fliplet.Widget.open('com.fliplet.file-picker', {
-  selector: '.file-picker-holder',
-  data: files,
-  onEvent: function(e, data) {
-    switch (e) {
-      case 'widget-rendered':
-        break;
-      case 'widget-set-info':
-        var msg = data.length ? data.length + ' files selected' : 'no selected files';
-        Fliplet.Widget.info(msg);
-        files.selectFiles = data.length ? data : [];
-        if (data.length) {
-          save();
-          if (imageEditorProvider) {
-            imageEditorProvider = null;
-            $('.image-editor-holder').html('');
-          }
-          initImageEditor();
-        }
-        break;
-      default:
-        break;
+function attahObservers() {
+  // Handle the tap_action change event
+  $('input[name="tap_action"]').on('change', function() {
+    if ($(this).val() === "link" && $(this).is(':checked')) {
+      if (!linkActionProvider) {
+        linkProviderInit();
+      }
+      $('.link-actions').addClass('show');
+      return;
     }
-  },
-  closeOnSave: false
-});
+    $('.link-actions').removeClass('show');
+  });
 
-filePickerProvider.then(function(data) {
-  Fliplet.Widget.info('');
-  files.selectFiles = data.data.length ? data.data : [];
-  save(true);
-});
+  // 1. Fired from Fliplet Studio when the external save button is clicked
+  Fliplet.Widget.onSaveRequest(function() {
+    if (linkActionProvider && !$('#pinch').is(':checked') && !$('#none').is(':checked')) {
+      linkActionProvider.forwardSaveRequest();
+    }
 
-function initImageEditor() {
+    if (filePickerProvider) {
+      filePickerProvider.forwardSaveRequest();
+      return;
+    }
+
+    save(true);
+  });
+
+  // Temporary alerts for Beta
+  $('#help_tip').on('click', function() {
+    alert("During beta, please use live chat and let us know what you need help with.");
+  });
+}
+
+function filePickerDataInit() {
+  filePickerData = $.extend(true, data.files, {
+    selectFiles: [],
+    selectMultiple: false,
+    type: 'image',
+    fileExtension: ['JPG', 'JPEG', 'PNG', 'GIF', 'TIFF'],
+    autoSelectOnUpload: true
+  });
+}
+
+function filePickerInit() {
+  filePickerDataInit();
+  if (filePickerProvider) {
+    filePickerProvider = null;
+    $('.file-picker-holder').html('');
+  }
+  filePickerProvider = Fliplet.Widget.open('com.fliplet.file-picker', {
+    selector: '.file-picker-holder',
+    data: filePickerData,
+    onEvent: function(e, data) {
+      switch (e) {
+        case 'widget-rendered':
+          break;
+        case 'widget-set-info':
+          var msg = data.length ? data.length + ' files selected' : 'no selected files';
+          Fliplet.Widget.info(msg);
+          filePickerData.selectFiles = data.length ? data : [];
+          if (data.length) {
+            save();
+            if (filePickerData.selectFiles[0].id !== data[0].id) {
+              imageEditorInit();
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    },
+    closeOnSave: false
+  });
+
+  filePickerProvider.then(function(data) {
+    Fliplet.Widget.info('');
+    filePickerData.selectFiles = data.data.length ? data.data : [];
+    save(true);
+  });
+}
+
+function imageEditorInit() {
+  if (!data.image) {
+    return;
+  }
+
+  if (imageEditorProvider) {
+    imageEditorProvider = null;
+    $('.image-editor-holder').html('');
+  }
   imageEditorProvider = Fliplet.Widget.open('com.fliplet.image-editor', {
     selector: '.image-editor-holder',
     data: {
@@ -84,13 +138,14 @@ function initImageEditor() {
   });
 
   imageEditorProvider.then(function(eventData) {
-    data.image = eventData.data.image;
+    filePickerData.selectFiles[0] = eventData.data.image;
     save();
+    filePickerInit();
   });
 }
 
 //mehtod used to init the link provider
-function initLinkProvider(linkAction) {
+function linkProviderInit(linkAction) {
   linkActionProvider = Fliplet.Widget.open('com.fliplet.link', {
     selector: '.link-actions',
     data: linkAction,
@@ -107,9 +162,9 @@ function initLinkProvider(linkAction) {
 }
 
 function save(notifyComplete) {
-  data.image = files.selectFiles[0].url ? files.selectFiles[0] : data.image && data.image.url ? data.image : null;
+  data.image = (filePickerData.selectFiles.length && filePickerData.selectFiles[0].url) ? filePickerData.selectFiles[0] : null;
   data.files = {
-    selectFiles: files.selectFiles.length ? files.selectFiles : data.image && data.image.url ? [data.image] : []
+    selectFiles: filePickerData.selectFiles.length ? filePickerData.selectFiles : data.image && data.image.url ? [data.image] : []
   };
 
   if (data.image && data.image.size) {
@@ -131,7 +186,7 @@ function save(notifyComplete) {
     data.action = null;
   }
 
-  Fliplet.Widget.save(data).then(function() {
+  return Fliplet.Widget.save(data).then(function() {
     if (notifyComplete) {
       Fliplet.Widget.complete();
       Fliplet.Studio.emit('reload-page-preview');
@@ -141,33 +196,4 @@ function save(notifyComplete) {
   });
 }
 
-//handle the tap_action change event
-$('input[name="tap_action"]').on('change', function() {
-  if ($(this).val() === "link" && $(this).is(':checked')) {
-    if (!linkActionProvider) {
-      initLinkProvider();
-    }
-    $('.link-actions').addClass('show');
-    return;
-  }
-  $('.link-actions').removeClass('show');
-});
-
-// 1. Fired from Fliplet Studio when the external save button is clicked
-Fliplet.Widget.onSaveRequest(function() {
-  if (linkActionProvider && !$('#pinch').is(':checked') && !$('#none').is(':checked')) {
-    linkActionProvider.forwardSaveRequest();
-  }
-
-  if (filePickerProvider) {
-    filePickerProvider.forwardSaveRequest();
-    return;
-  }
-
-  save(true);
-});
-
-// Temporary alerts for Beta
-$('#help_tip').on('click', function() {
-  alert("During beta, please use live chat and let us know what you need help with.");
-});
+init();
