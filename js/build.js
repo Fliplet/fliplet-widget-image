4e7270162c91b75ba8eb60cf4490a6d6f5764d4a
Veljko Popović <50653475+veljko7@users.noreply.github.com>
2024-04-29 11:34:04 +0200

    ID-4289 [Feature] Dynamic image component as v2.0.0 (#65)
    * Initial commit
    
    * image component in progress
    
    * First prototype
    
    * Image component finished
    
    Interface, theme settings and build.js
    
    * image component refactored
    
    Code refactoring
    
    * image component
    
    * image component finished
    
    * List repeater package name changed
    
    * testing on event
    
    this will be removed
    
    * reverted after testing
    
    * PR review
    
    * PR review
    
    * PR review
    
    * PR review
    
    * PR review
    
    * PR review
    
    * PR review
    
    * PR review
    
    * Add lazy loading to image element
    
    * Ensures new widget version works alongside the other versions
    
    ---------
    
    Co-authored-by: Tony Wu <tonytlwu@users.noreply.github.com>

---
+++ js/build.js
@@ -0,0 +1,87 @@
+Fliplet.Widget.instance({
+  name: 'image-2-0-0',
+  displayName: 'Dynamic image',
+  render: {
+    ready: async function() {
+      const image = this;
+      const entryData = image?.parent?.entry?.data || {};
+      const imageInstanceId = image.id;
+      const $imageContainer = $(image.$el);
+
+      image.fields = _.assign(
+        {
+          showIfImageNotFound: 'placeholder',
+          imageColumnName: ''
+        },
+        image.fields
+      );
+
+      let imageOptions = {
+        showPlaceholder: false,
+        url: null,
+        alt: 'Image placeholder',
+        imageColumnName: image.fields.imageColumnName,
+        showIfImageNotFound: image.fields.showIfImageNotFound,
+        placeholderPath: Fliplet.Widget.getAsset(imageInstanceId, 'img/placeholder.jpg')
+      };
+
+      if (Fliplet.Env.get('mode') === 'interact' || !imageOptions.imageColumnName) {
+        imageOptions.showPlaceholder = true;
+
+        return renderImage();
+      }
+
+      Fliplet.Widget.findParents({ instanceId: imageInstanceId }).then(function(widgets) {
+        let dynamicContainer = null;
+        let recordContainer = null;
+        let listRepeater = null;
+
+        widgets.forEach(widget => {
+          if (widget.package === 'com.fliplet.dynamic-container') {
+            dynamicContainer = widget;
+          } else if (widget.package === 'com.fliplet.record-container') {
+            recordContainer = widget;
+          } else if (widget.package === 'com.fliplet.list-repeater') {
+            listRepeater = widget;
+          }
+        });
+
+        if (!dynamicContainer || !dynamicContainer.dataSourceId || (!recordContainer && !listRepeater)) {
+          return;
+        }
+
+        renderImage();
+      });
+
+
+      function renderImage() {
+        const imageColumnUrlValue = entryData[imageOptions.imageColumnName];
+
+        if (imageOptions.showPlaceholder) {
+          imageOptions.url = imageOptions.placeholderPath;
+        } else if (imageColumnUrlValue?.length) {
+          if (Array.isArray(imageColumnUrlValue)) {
+            imageOptions.url = imageColumnUrlValue[0];
+          } else {
+            imageOptions.url = imageColumnUrlValue;
+          }
+
+          imageOptions.alt = 'Image';
+        } else if (imageOptions.showIfImageNotFound === 'placeholder') {
+          imageOptions.url = imageOptions.placeholderPath;
+        } else {
+          return; // Exit early if image doesn't exist and placeholder shouldn't be shown
+        }
+
+        let img = document.createElement('img');
+
+        img.loading = 'lazy';
+        img.setAttribute('data-image-id', imageInstanceId);
+        img.alt = imageOptions.alt;
+        img.src = imageOptions.url;
+
+        $imageContainer.html(img);
+      }
+    }
+  }
+});
---
+++ js/interface.js
@@ -0,0 +1,66 @@
+
+Fliplet.Widget.findParents().then(function(widgets) {
+  let dynamicContainer = null;
+  let recordContainer = null;
+  let listRepeater = null;
+
+  widgets.forEach(widget => {
+    if (widget.package === 'com.fliplet.dynamic-container') {
+      dynamicContainer = widget;
+    } else if (widget.package === 'com.fliplet.record-container') {
+      recordContainer = widget;
+    } else if (widget.package === 'com.fliplet.list-repeater') {
+      listRepeater = widget;
+    }
+  });
+
+  const dataSourceId = dynamicContainer?.dataSourceId;
+
+  if (!dynamicContainer || !dataSourceId || (!recordContainer && !listRepeater)) {
+    return;
+  }
+
+  return Fliplet.DataSources.getById(dataSourceId, {
+    attributes: ['name', 'columns']
+  }).then(async function(dataSource) {
+    const dataSourceColumns = dataSource.columns;
+    const dataSourceName = dataSource.name;
+
+    return Fliplet.Widget.generateInterface({
+      title: 'Dynamic image',
+      supportUrl: 'https://www.google.com', // TODO missing link
+      fields: [
+        {
+          type: 'html',
+          html: `<div>
+              <h3 style="color: black; font-weight: 600;">Get image from</h3>
+              <p style="margin-bottom: 5px;">
+                <span style="color: red;">${dataSourceId} </span>
+                <span style="color: black; font-weight: 600;">${dataSourceName}</span>
+              </p>
+              <p style="color: grey; font-size: 10px; margin-bottom: 25px;">To change this Data Source, go to Parent data container</p>
+            </div>`
+        },
+        {
+          name: 'imageColumnName',
+          type: 'dropdown',
+          label: 'Select image column',
+          options: dataSourceColumns,
+          default: ''
+        },
+        {
+          name: 'showIfImageNotFound',
+          type: 'radio',
+          label: 'If no image found',
+          options: [
+            { value: 'placeholder', label: 'Show placeholder' },
+            { value: 'none', label: 'None' }
+          ],
+          default: 'placeholder'
+        }
+      ]
+    });
+  });
+});
+
+
